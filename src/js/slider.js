397a8d4fd225b96688d291a3a78d7cf790e0cc4d
Viacheslav-Nikolaev <n.viacheslav7127@yandex.ru>
2021-03-22 21:39:08 +0300

    new commit

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,29 @@
-export let swiper; 
+export let swiperOne;
+export let swiperTwo;
 
-export function swiperStart() {
-   swiper = new Swiper('.slider', {
+export function swiperStartOne(swiperClass) {
+   
+   swiperOne = new Swiper(swiperClass, {
+      slidesPerView: 'auto',
+      centeredSlides: false,
+      spaceBetween: 16,
+      setWrapperSize: true,
+      pagination: {
+         el: '.swiper-pagination',
+         type: 'bullets',
+         clickable: true,
+      }
+   });
+}
+
+export function swiperDestroy(swiper) {
+   swiper.destroy()
+}
+
+
+
+export function swiperStartTwo(swiperClass) {
+   swiperTwo = new Swiper(swiperClass, {
       slidesPerView: 'auto',
       centeredSlides: false,
       spaceBetween: 16,
@@ -15,16 +37,32 @@ export function swiperStart() {
 }
 
 
-export function changes(screen) {
+export function changesOne(screen) {
    if (!screen.matches) {
-      swiperStart();
-   } else if (swiper !== undefined) {
-      swiper.destroy();
+      swiperStartOne('.repair-brand__menu');
+   } else if (swiperOne !== undefined) {
+      swiperDestroy(swiperOne);
+   }
+}
+
+export function changesTwo(screen) {
+   if (!screen.matches) {
+      swiperStartTwo('.repair-types__menu');
+   } else if (swiperOne !== undefined) {
+      swiperDestroy(swiperTwo);
    }
 }
 
 
+
 export let screen = window.matchMedia('(min-width: 768px)');
-screen.addListener(changes);
-changes(screen);
+screen.addListener(changesOne);
+screen.addListener(changesTwo);
+changesOne(screen);
+changesTwo(screen);
+
+
+
+
+
 
